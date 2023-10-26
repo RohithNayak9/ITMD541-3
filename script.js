638e0a20f2b01861648d495166bba20620998ae0
RohithNayak9 <145180389+RohithNayak9@users.noreply.github.com>
2023-10-26 00:22:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const billTotalInput = document.getElementById('billTotal');
+    const tipInput = document.getElementById('tip');
+    const tipPercentageInput = document.getElementById('tipPercentage');
+    const tipAmountInput = document.getElementById('tipAmount');
+    const totalBillWithTipInput = document.getElementById('totalBillWithTip');
+
+    const form = document.getElementById('tipForm');
+
+    form.addEventListener('input', function() {
+        let bill = parseFloat(billTotalInput.value);
+        let tipPercentage = tipInput.value;
+        
+        if (isNaN(bill)) {
+            alert('Please enter a valid number for the bill total.');
+            return;
+        }
+        
+        tipPercentageInput.value = tipPercentage;
+        let tipAmount = (bill * tipPercentage) / 100;
+        tipAmountInput.value = tipAmount.toFixed(2);
+        totalBillWithTipInput.value = (bill + tipAmount).toFixed(2);
+    });
+});
+
+function refreshPage() {
+    window.location.reload();
+}
+
+function showNotification() {
+    alert('Paid successfully');
+    window.location.reload();
+}
